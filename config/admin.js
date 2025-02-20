d7580347f9ba16daed586f8fcb5b3c9110129aa3
Isaac Shed <isaacsheds@gmail.com>
2025-02-20 21:23:30 +0100

    let give it a go with pnpm

--- config/admin.js
+++ config/admin.js
@@ -1,45 +1,3 @@
-// module.exports = ({ env }) => ({
-//   auth: {
-//     secret: env('ADMIN_JWT_SECRET'),
-//   },
-//   apiToken: {
-//     salt: env('API_TOKEN_SALT'),
-//   },
-//   transfer: {
-//     token: {
-//       salt: env('TRANSFER_TOKEN_SALT'),
-//     },
-//   },
-//   flags: {
-//     nps: env.bool('FLAG_NPS', true),
-//     promoteEE: env.bool('FLAG_PROMOTE_EE', true),
-//   },
-//   build: {
-//     config: {
-//       plugins: {
-//         vite: {
-//           build: {
-//             rollupOptions: {
-//               external: [
-//                 '@strapi/design-system/v2',
-//                 '@strapi/design-system',
-//                 '@strapi/icons',
-//                 '@strapi/helper-plugin'
-//               ]
-//             }
-//           },
-//           resolve: {
-//             alias: {
-//               '@strapi/design-system/v2': '@strapi/design-system',
-//               '@strapi/design-system': '@strapi/design-system'
-//             }
-//           }
-//         }
-//       }
-//     }
-//   }
-// });
-
 module.exports = ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET'),
@@ -58,10 +16,52 @@ module.exports = ({ env }) => ({
   },
   build: {
     config: {
-      additionalWatchIgnores: ['**/outputs/**'],
-      optimizeDeps: {
-        include: ['@strapi/design-system', '@strapi/icons']
+      plugins: {
+        vite: {
+          build: {
+            rollupOptions: {
+              external: [
+                '@strapi/design-system/v2',
+                '@strapi/design-system',
+                '@strapi/icons',
+                '@strapi/helper-plugin'              
+              ]
+            }
+          },
+          resolve: {
+            alias: {
+              '@strapi/design-system/v2': '@strapi/design-system',
+              '@strapi/design-system': '@strapi/design-system'
+            }
+          }
+        }
       }
     }
   }
 });
+
+// module.exports = ({ env }) => ({
+//   auth: {
+//     secret: env('ADMIN_JWT_SECRET'),
+//   },
+//   apiToken: {
+//     salt: env('API_TOKEN_SALT'),
+//   },
+//   transfer: {
+//     token: {
+//       salt: env('TRANSFER_TOKEN_SALT'),
+//     },
+//   },
+//   flags: {
+//     nps: env.bool('FLAG_NPS', true),
+//     promoteEE: env.bool('FLAG_PROMOTE_EE', true),
+//   },
+//   build: {
+//     config: {
+//       additionalWatchIgnores: ['**/outputs/**'],
+//       optimizeDeps: {
+//         include: ['@strapi/design-system', '@strapi/icons']
+//       }
+//     }
+//   }
+// });
--- config/plugins.js
+++ config/plugins.js
@@ -1,19 +1,40 @@
+// module.exports = ({ env }) => ({
+//   upload: {
+//     config: {
+//       provider: 'local',
+//       providerOptions: {
+//         sizeLimit: 100 * 1024 * 1024, // 100mb
+//       },
+//       breakpoints: {
+//         xlarge: 1920,
+//         large: 1000,
+//         medium: 750,
+//         small: 500,
+//       },
+//       processor: {
+//         sharp: false,
+//         jimp: true
+//       }
+//     },
+//   },
+//   tinymce: {
+//     enabled: true
+//   }
+// });
 module.exports = ({ env }) => ({
-  tinymce: {
-    enabled: true
-  },
   upload: {
     config: {
+      sizeLimit: 250 * 1024 * 1024,
+      provider: 'local',
       providerOptions: {
-        sizeLimit: 250 * 1024 * 1024,
+        localServer: {
+          maxage: 300000
+        },
       },
-      breakpoints: {
-        xlarge: 1920,
-        large: 1000,
-        medium: 750,
-        small: 500,
-        xsmall: 64
-      },
+      breakpoints: false
     },
   },
+  tinymce: {
+    enabled: true
+  }
 });
